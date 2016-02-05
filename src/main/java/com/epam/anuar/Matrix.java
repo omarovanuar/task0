cca26f64108dc520b19036eefe500d94a57364b7
anuar <omarovanuar91@gmail.com>
2016-02-05 06:33:43 +0600

    Finish project

--- src/main/java/com/epam/anuar/Matrix.java
+++ src/main/java/com/epam/anuar/Matrix.java
@@ -1,34 +1,46 @@
 package com.epam.anuar;
 
-/**
- * Created by Ануар on 04.02.2016.
- */
 public class Matrix {
     int[][] values;
-    int row;
-    int column;
+    int row = 3;
+    int column = 3;
 
+    Matrix(){
+        values = new int[row][column];
+    }
 
     Matrix(int rows, int columns){
+        values = new int[rows][columns];
         row = rows;
         column = columns;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
-                values[i][j] = (int) (Math.random() * 10);
+                values[i][j] = (int) (Math.random() *10);
             }
         }
     }
 
 
 
-    public Matrix multiply(Matrix matrix2, Matrix matrixEnd) {
-        for (int i = 0; i < row; i++) {
+    public Matrix multiply(Matrix matrix1, Matrix matrix2, Matrix matrixEnd) {
+        for (int i = 0; i < matrix1.row; i++) {
             for (int j = 0; j < matrix2.column; j++) {
-                for (int k = 0; k < column; k++) {
-                    matrixEnd.values[i][j] += values[j][k] * matrix2.values[k][j];
+                for (int k = 0; k < matrix1.column; k++) {
+                    values[i][j] += matrix1.values[i][k] * matrix2.values[k][j];
                 }
             }
         }
         return matrixEnd;
+    }
+
+
+    public void showMatrixValues(int rows, int columns) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(values[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
--- src/main/java/com/epam/anuar/main.java
+++ src/main/java/com/epam/anuar/main.java
@@ -7,9 +7,9 @@ public class main {
     public static void main(String[] args) {
         Matrix matrix1 = new Matrix(3, 3);
         Matrix matrix2 = new Matrix(3, 3);
-        Matrix matrixEnd = new Matrix(matrix1.row, matrix2.column);
-        matrixEnd = matrix1.multiply(matrix2, matrixEnd);
-        System.out.println(matrixEnd.values);
+        Matrix matrixEnd = new Matrix();
+        matrixEnd = matrixEnd.multiply(matrix1, matrix2, matrixEnd);
+        matrixEnd.showMatrixValues(matrixEnd.row, matrixEnd.column);
 
     }
 }
